05e0c04f6904dc2583424a2fd473fc387e7c70f5
Mara <chonvon2021@gmail.com>
2024-10-27 22:02:40 +0700

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,21 @@
+document.addEventListener('mousemove', e => {
+    Object.assign(document.documentElement, {
+        style: `
+        --move-x: ${(e.clientX - window.innerWidth / 2) * -.005}deg;
+        --move-y: ${(e.clientY - window.innerHeight / 2) * -.01}deg;
+        `
+    })
+})
+
+document.querySelectorAll('.slider').forEach((n, i) => {
+	window[`slider${i+1}`] = new Swiper(n, {
+		freeMode: true,
+		centeredSlides: true,
+		direction: 'vertical',
+		mousewheel: true,
+		slidesPerView: 1.75,
+		slidesOffsetBefore: -125
+	})
+})
+bindSwipers(slider1, slider2, slider3, slider4)
+
